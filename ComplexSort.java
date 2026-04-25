07593e8efb0c4c0268972dcfbaf95e63226d96c0
Sirisha Cherala <sirishacherala@gmail.com>
2026-04-25 14:22:34 -0700

    Implement quick sort...

--- ComplexSort.java
+++ ComplexSort.java
@@ -2,10 +2,16 @@ import java.util.*;
 
 public class ComplexSort {
     public static void sort(List<Integer> arr) {
-        selectionSort(arr);
+        if (arr.size() == 7) {
+            selectionSort(arr);
+        }
+        else {
+            quickSort(arr);
+        }
     }
 
     private static void selectionSort(List<Integer> arr) {
+        System.out.println("Selection sort....");
         selectionSort(arr, 0, arr.size() - 1);
     }
 
@@ -20,6 +26,35 @@ public class ComplexSort {
 
             swap(arr, i, minInd);
         }
+    }
+
+    private static void quickSort(List<Integer> arr) {
+        System.out.println("Quick sort....");
+        quickSort(arr, 0, arr.size() - 1);
+    }
+
+    private static void quickSort(List<Integer> arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int k = quickSelect(arr, start, end);
+        quickSort(arr, start, k - 1);
+        quickSort(arr, k + 1, end);
+    }
+
+    private static int quickSelect(List<Integer> arr, int start, int end) {
+        int index = start;
+        int current = arr.get(end);
+        for (int i = start; i < end; i++) {
+            if (arr.get(i) < current) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+
+        swap(arr, index, end);
+        return index;
     }
 
     private static void swap(List<Integer> arr, int first, int second) {
--- ComplexSortTester.java
+++ ComplexSortTester.java
@@ -2,15 +2,24 @@ import java.util.*;
 
 public class ComplexSortTester {
     public static void main (String[] args) {
-        List<Integer> arr = new ArrayList<>();
-        arr.addAll(Arrays.asList(5,3,2,6,1,4,0));
+        List<List<Integer>> arr = new ArrayList<>();
 
-        ComplexSort.sort(arr);
+        arr.add(Arrays.asList(5,3,2,6,1,4,0));
+        arr.add(Arrays.asList(7,5,3,2,8,6,1,9,4,0));
 
-        for (int i = 0;i < arr.size() - 1;i++) {
-            assert(arr.get(i) < arr.get(i+1));
+
+        for (List<Integer> list : arr) {
+            ComplexSort.sort(list);
+            assertSortedArray(list);
+            System.out.println("Sorted list successfully: " + list);
         }
 
-        System.out.println("Sorted arr successfully");
+
+    }
+
+    private static void assertSortedArray(List<Integer> arr) {
+        for(int i = 0; i < arr.size() - 1;i++) {
+            assert(arr.get(i) < arr.get(i+1));
+        }
     }
 }
